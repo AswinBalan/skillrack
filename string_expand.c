2413150799f58bc0efc3f8937e912eb5ecda997b
Aswin <aswindpsp15@gmail.com>
2018-07-28 14:11:52 +0530

    string expansion

---
+++ string_expand.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{ char s[10];
+   int n,i;
+   while(scanf("%[^0-9\n]%d",&s,&n)){
+   	for(i=0;i<n;i++){
+   		printf("%s",s);
+   	}
+   }
+   }
+
+//string expansion
+//as2de3ss2 --input
+//asasdededessss --output
